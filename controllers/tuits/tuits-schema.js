e4542ec3b6d5b2b34a2dcd5213010d737c57bc2f
z-roth <zack27r@gmail.com>
2022-11-30 22:49:10 -0500

    Trying this one more time...

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,18 @@
-import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-  tuit: String,
-  likes: Number,
-  liked: Boolean,
-}, {collection: 'tuits'});
+import mongoose from "mongoose";
+const schema = mongoose.Schema(
+  {
+    handle: String,
+    username: String,
+    image: String,
+    tuit: String,
+    time: String,
+    likes: Number,
+    liked: Boolean,
+    dislikes: Number,
+    disliked: Boolean,
+    replies: Number,
+    retuits: Number,
+  },
+  { collection: "tuits" }
+);
 export default schema;
